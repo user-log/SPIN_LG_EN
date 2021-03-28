c239d9b2b28da0ec92efba4c4f394ff2c8e32412
user-log <76106375+user-log@users.noreply.github.com>
2021-03-28 03:27:06 +0200

    Update langs.php

--- langs.php
+++ langs.php
@@ -6,55 +6,55 @@
     $lang_en = array(
 
         '1' => 'SPIN TO WIN',
-        '2' => "Please fill out the registration form <a href='https://bit.ly/2PvB6fg'>HERE</a> <meta http-equiv='refresh' content='7; URL=https://bit.ly/2PvB6fg' />"
+        '2' => "Please fill out the registration form <a href='https://bit.ly/3rpxSHb'>HERE</a> <meta http-equiv='refresh' content='7; URL=https://bit.ly/3rpxSHb' />"
     );
 
     $lang_id = array(
 
         '1' => 'klik disini',
-        '2' => "Silakan isi formulir pendaftaran <a href='https://bit.ly/2PvB6fg'>dari sini</a> <meta http-equiv='refresh' content='7; URL=https://bit.ly/2PvB6fg' />"
+        '2' => "Silakan isi formulir pendaftaran <a href='https://bit.ly/3rpxSHb'>dari sini</a> <meta http-equiv='refresh' content='7; URL=https://bit.ly/3rpxSHb' />"
     );
 
     $lang_pt = array(
 
         '1' => 'Clique aqui',
-        '2' => "Por favor, preencha o formulário de inscrição <a href='https://bit.ly/2PvB6fg'>daqui</a> <meta http-equiv='refresh' content='7; URL=https://bit.ly/2PvB6fg' />"
+        '2' => "Por favor, preencha o formulário de inscrição <a href='https://bit.ly/3rpxSHb'>daqui</a> <meta http-equiv='refresh' content='7; URL=https://bit.ly/3rpxSHb' />"
     );
 
     $lang_es = array(
 
         '1' => 'rueda giratoria',
-        '2' => "Por favor complete el formulario de registro <a href='https://bit.ly/2PvB6fg'>de aquí</a> <meta http-equiv='refresh' content='7; URL=https://bit.ly/2PvB6fg' />"
+        '2' => "Por favor complete el formulario de registro <a href='https://bit.ly/3rpxSHb'>de aquí</a> <meta http-equiv='refresh' content='7; URL=https://bit.ly/3rpxSHb' />"
     );
 
     $lang_fr = array(
 
         '1' => 'Cliquez sur',
-        '2' => "Veuillez remplir le formulaire d'inscription <a href='https://bit.ly/2PvB6fg'>d'ici</a> <meta http-equiv='refresh' content='7; URL=https://bit.ly/2PvB6fg' />"
+        '2' => "Veuillez remplir le formulaire d'inscription <a href='https://bit.ly/3rpxSHb'>d'ici</a> <meta http-equiv='refresh' content='7; URL=https://bit.ly/3rpxSHb' />"
     );
 
     $lang_tr = array(
 
         '1' => 'Tıklayın',
-        '2' => "Lütfen kayıt formunu doldurun <a href='https://bit.ly/2PvB6fg'>buradan</a> <meta http-equiv='refresh' content='7; URL=https://bit.ly/2PvB6fg' />"
+        '2' => "Lütfen kayıt formunu doldurun <a href='https://bit.ly/3rpxSHb'>buradan</a> <meta http-equiv='refresh' content='7; URL=https://bit.ly/3rpxSHb' />"
     );
 
     $lang_fi = array(
 
         '1' => 'pindutin dito',
-        '2' => "Mangyaring punan ang form sa pagpaparehistro <a href='https://bit.ly/2PvB6fg'>mula rito</a> <meta http-equiv='refresh' content='7; URL=https://bit.ly/2PvB6fg' />"
+        '2' => "Mangyaring punan ang form sa pagpaparehistro <a href='https://bit.ly/3rpxSHb'>mula rito</a> <meta http-equiv='refresh' content='7; URL=https://bit.ly/3rpxSHb' />"
     );
 
     $lang_ur = array(
 
         '1' => 'سپن وہیل',
-        '2' => "برائے کرم رجسٹریشن فارم پُر کریں <a href='https://bit.ly/2PvB6fg'>یہاں سے</a> <meta http-equiv='refresh' content='7; URL=https://bit.ly/2PvB6fg' />"
+        '2' => "برائے کرم رجسٹریشن فارم پُر کریں <a href='https://bit.ly/3rpxSHb'>یہاں سے</a> <meta http-equiv='refresh' content='7; URL=https://bit.ly/3rpxSHb' />"
     );
     
     $lang_ar = array(
 
         '1' => 'أدر العجلة',
-        '2' => "يرجى ملء استمارة التسجيل من <a href='https://bit.ly/2PvB6fg'>هنا</a> <meta http-equiv='refresh' content='7; URL=https://bit.ly/2PvB6fg' />"
+        '2' => "يرجى ملء استمارة التسجيل من <a href='https://bit.ly/3rpxSHb'>هنا</a> <meta http-equiv='refresh' content='7; URL=https://bit.ly/3rpxSHb' />"
     );
 
     
@@ -62,7 +62,7 @@
     $lang_pa = array(
         
         '1' => 'ਇੱਥੇ ਕਲਿੱਕ ਕਰੋ',
-        '2' => "ਕਿਰਪਾ ਕਰਕੇ ਰਜਿਸਟ੍ਰੇਸ਼ਨ ਫਾਰਮ ਭਰੋ <a href='https://bit.ly/2PvB6fg'>ਇਥੋਂ</a> <meta http-equiv='refresh' content='7; URL=https://bit.ly/2PvB6fg' />"
+        '2' => "ਕਿਰਪਾ ਕਰਕੇ ਰਜਿਸਟ੍ਰੇਸ਼ਨ ਫਾਰਮ ਭਰੋ <a href='https://bit.ly/3rpxSHb'>ਇਥੋਂ</a> <meta http-equiv='refresh' content='7; URL=https://bit.ly/3rpxSHb' />"
     );
 
 
